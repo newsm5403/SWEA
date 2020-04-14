46cf6c47d457f98e8ac53ebf2632eeb5f35ced78
newsm5403 <newsm5403@naver.com>
2020-04-14 18:33:59 +0900

    수정본

--- SWEA 2058 자릿수 더하기.py
+++ SWEA 2058 자릿수 더하기.py
@@ -1,9 +1,16 @@
 N = int(input())  # 정수형 입력받기
+n = 0
 dummy = N
 sum_N = 0
-i = 3
-while i >= 0:
-    sum_N += dummy // pow(10,i)
-    dummy = N % pow(10,i)
-    i -= 1
+
+while True:       # 정수 자리수 구하기
+    result = N // pow(10, n)
+    if result == 0:
+        break
+    n += 1
+
+while n >= 0:     # 정수 자리수 합 구하기
+    sum_N += dummy // pow(10, n)
+    dummy = N % pow(10, n)
+    n -= 1
 print(sum_N)
