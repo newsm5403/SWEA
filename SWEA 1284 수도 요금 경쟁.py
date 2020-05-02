1345b49353898178115be29f26e88103d35abc07
newsm5403 <newsm5403@naver.com>
2020-05-02 16:37:16 +0900

    D+6 코드 간략화

--- SWEA 1284 수도 요금 경쟁.py
+++ SWEA 1284 수도 요금 경쟁.py
@@ -1,12 +1,3 @@
-for i in range(int(input())):
-    a = list(map(int, input().split()))
-    SUM1 = a[4] * a[0]
-    if a[4] > a[2]:
-        SUM2 = a[1] + (a[4] - a[2]) * a[3]
-    else:
-        SUM2 = a[1]
-    if SUM1 > SUM2:
-        SUM = SUM2
-    elif SUM2 > SUM1:
-        SUM = SUM1
-    print(f'#{i+1} {SUM}')
+for t in range(int(input())):
+    P, Q, R, S, W = map(int,input().split())
+    print(f'#{t+1}', min(W*P, Q+max(0, (W-R)*S)))
